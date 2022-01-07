21e3bf82ad37114b1b58255b19e8fec42d6a0c93
Biruk Endris <57604289+Biruk-hub@users.noreply.github.com>
2022-01-07 14:25:14 +0100

    fetch timeout functionality added
    on this code i added setTimeout to fetch the api whenever the user left the keyboard, also i added a feature to avoid space when there is to many spaces

--- Fetch Api/script2.js
+++ Fetch Api/script2.js
@@ -1,46 +1,54 @@
-const baseShowUrl = 'https://api.tvmaze.com/search/shows?q='
+const baseShowUrl = "https://api.tvmaze.com/search/shows?q=";
 function searchShow(query) {
-    fetch(baseShowUrl + query)
-    .then(response => response.json())
-    .then(data => {
-        // showNames = data.map((element) => {
-        //     return element.show.name;
-        // })
-        // console.log(showNames);
-        updateList(data);
+  fetch(baseShowUrl + query)
+    .then((response) => response.json())
+    .then((data) => {
+      // showNames = data.map((element) => {
+      //     return element.show.name;
+      // })
+      // console.log(showNames);
+      updateList(data);
     })
-    .catch(err => {
-        console.log(`Error: ${err}`);
-    })
+    .catch((err) => {
+      console.log(`Error: ${err}`);
+    });
 }
 
 // Create input element
-const textInput = document.createElement('input');
-textInput.type = 'text';
-textInput.placeholder = 'Search any show';
+const textInput = document.createElement("input");
+textInput.type = "text";
+textInput.placeholder = "Search any show";
+
+// manage fetching time out
+let setTimeOutToken = 0;
+
 textInput.onkeyup = () => {
-    if(textInput.value.length > 0) {
-        searchShow(textInput.value);
-    }
-}
+  if (textInput.value.trim().length > 0) {
+    //   let's clear the previous timer to fetch the data
+    clearTimeout(setTimeOutToken);
+    setTimeOutToken = setTimeout(() => {
+      searchShow(textInput.value);
+    }, 250);
+  }
+};
+
 // Create list element
-const list = document.createElement('ul');
+const list = document.createElement("ul");
 
 // Update the list element
 function updateList(data) {
-    list.innerHTML = '';
-    data.forEach(element => {
-        const li = document.createElement('li');
-        const a = document.createElement('a');
-        a.href = element.show.url;
-        a.innerText = element.show.name;
-        li.appendChild(a);
-        list.appendChild(li);
-    })
+  list.innerHTML = "";
+  data.forEach((element) => {
+    const li = document.createElement("li");
+    const a = document.createElement("a");
+    a.href = element.show.url;
+    a.innerText = element.show.name;
+    li.appendChild(a);
+    list.appendChild(li);
+  });
 }
-
 
 window.onload = () => {
-    document.body.appendChild(textInput);
-    document.body.appendChild(list);
-}
+  document.body.appendChild(textInput);
+  document.body.appendChild(list);
+};
